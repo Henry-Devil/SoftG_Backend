81459e007bc832cfdebb2501d222e2dcd4c97eec
Henry Viloria <henrimavi123@gmail.com>
2023-03-08 18:50:53 -0500

    importamos validateToken y lo agregamos en .get

--- src/routes/driver.ts
+++ src/routes/driver.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { getDrivers } from "../controllers/driver";
+import validateToken from "./validate-token";
 
 const router = Router();
 
-router.get('/', getDrivers)
+router.get('/', validateToken, getDrivers)
 export default router;
