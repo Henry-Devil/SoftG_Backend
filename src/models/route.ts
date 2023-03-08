2986bed193bf91c67ae98807660fda4147af2fb2
Henry Viloria <henrimavi123@gmail.com>
2023-03-08 17:51:52 -0500

    Se hizo conexión de tablas

--- src/models/route.ts
+++ src/models/route.ts
@@ -1,5 +1,8 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../db/connection";
+import { Driver } from "./driver";
+import { Vehicle } from "./vehicle";
+import { Schedule } from "./schedule";
 
 export const Route = sequelize.define('route', {
     id: {
@@ -14,4 +17,17 @@ export const Route = sequelize.define('route', {
 
     //vehicles_id
     
-},)
+});
+
+
+// asociación con la tabla drivers
+Route.belongsTo(Driver, { foreignKey: 'drivers_id' });
+Driver.hasMany(Route, { foreignKey: 'drivers_id' });
+
+// asociación con la tabla vehicles
+Route.belongsTo(Vehicle, { foreignKey: 'vehicles_id' });
+Vehicle.hasMany(Route, { foreignKey: 'vehicles_id' });
+
+// asociación con la tabla schedules
+Route.hasMany(Schedule, { foreignKey: 'route_id' });
+Schedule.belongsTo(Route, { foreignKey: 'route_id' });
