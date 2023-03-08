105dfeb0e26924efc640641abc54779063d49a11
Henry Viloria <henrimavi123@gmail.com>
2023-03-08 17:23:31 -0500

    Creamos modelos

--- src/models/server.ts
+++ src/models/server.ts
@@ -1,11 +1,63 @@
+import express, { Application } from 'express';
+import routesDriver from '../routes/driver';
+import routesUser from '../routes/user';
+import routesVehicles from '../routes/vehicle';
+import routesRoutes from '../routes/route'
+import routesSchedules from '../routes/schedule'
+import { Driver } from './driver';
+import { User } from './user';
+import { Vehicle } from './vehicle';
+import { Route } from './route';
+import { Schedule } from './schedule';
 
 
 class Server {
+    private app: Application;
+    private port: string;
 
     constructor (){
-        console.log('Estoy en el constructor');
-        
+        this.app = express();
+        this.port = process.env.PORT || '3001';
+        this.listen();
+        this.midlwares();
+        this.routes();
+        this.dbConect()
     }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log('Aplicación corriendo en puerto ' + this.port )
+            
+        })
+    }
+
+    routes(){
+        this.app.use('/api/drivers', routesDriver);
+        this.app.use('/api/users', routesUser);
+        this.app.use('/api/vehicles', routesVehicles);
+        this.app.use('/api/routes', routesRoutes);
+        this.app.use('/api/schedules', routesSchedules)
+    }
+
+    midlwares(){
+        this.app.use(express.json());
+    }
+
+    async dbConect(){
+        try {
+            await Driver.sync()
+            await User.sync()
+            await Vehicle.sync()
+            await Route.sync()
+            await Schedule.sync()
+        } catch (error) {
+            console.log('Unable to connect to the database.', error);
+            
+        }
+    }
+
+    
+
 }
 
 export default Server
