27a6b2736dae58995c1f49dadb2f26f6391e5946
Henry Viloria <henrimavi123@gmail.com>
2023-03-08 23:29:45 -0500

    creación de rutas

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getRoutes } from "../controllers/route";
+import { deleteRoute, getRoute, getRoutes, postRoute, putRoute } from "../controllers/route";
 import validateToken from "./validate-token";
 
 const router = Router();
 
 router.get('/',validateToken, getRoutes)
+router.get('/:id', getRoute);
+router.post('/', postRoute);
+router.put('/:id', putRoute);
+router.delete('/:id', deleteRoute);
 export default router;
