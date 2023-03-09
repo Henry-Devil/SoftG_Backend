b722fb7be225fcada47c16c37c72ad85abaa91e9
Henry Viloria <henrimavi123@gmail.com>
2023-03-08 23:29:28 -0500

    Crud ORM de route

--- src/controllers/route.ts
+++ src/controllers/route.ts
@@ -8,4 +8,61 @@ export const getRoutes = async (req: Request, res: Response) => {
 
 } 
 
+// Obtener una ruta por ID
+export const getRoute = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const route = await Route.findByPk(id);
+    if (!route) {
+      res.status(404).json({ msg: 'La ruta no existe' });
+      return;
+    }
+    res.json(route);
+  };
 
+// Eliminar una ruta existente
+export const deleteRoute = async (req: Request, res: Response) => {
+    const { id } = req.params;
+  
+    try {
+      const route = await Route.findByPk(id);
+      if (!route) {
+        res.status(404).json({ msg: 'La ruta no existe' });
+        return;
+      }
+      await route.destroy();
+      res.json({ msg: 'Ruta eliminada exitosamente' });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ msg: 'Ocurrió un error al eliminar la ruta' });
+    }
+  };
+
+// Crear una nueva ruta
+export const postRoute = async (req: Request, res: Response) => {
+    const { body } = req;
+    try {
+      const route = await Route.create(body);
+      res.json(route);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ msg: 'Ocurrió un error al crear la ruta' });
+    }
+  };
+
+  export const putRoute = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { body } = req;
+  
+    try {
+      const route = await Route.findByPk(id);
+      if (!route) {
+        res.status(404).json({ msg: 'La ruta no existe' });
+        return;
+      }
+      await route.update(body);
+      res.json(route);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ msg: 'Ocurrió un error al actualizar la ruta' });
+    }
+  };
