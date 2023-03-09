1df3d0cea9bd54692ab066c644583c2143512f8c
Henry Viloria <henrimavi123@gmail.com>
2023-03-08 23:44:32 -0500

    asignación de rutas

--- src/routes/schedule.ts
+++ src/routes/schedule.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getSchedules } from "../controllers/schedule";
+import { deleteSchedule, getSchedule, getSchedules, postSchedule, putSchedule } from "../controllers/schedule";
 import validateToken from "./validate-token";
 
 const router = Router();
 
-router.get('/',validateToken, getSchedules)
+router.get('/',validateToken, getSchedules);
+router.get('/:id', getSchedule);
+router.post('/', postSchedule);
+router.put('/:id', putSchedule);
+router.delete('/:id', deleteSchedule);
 export default router;
