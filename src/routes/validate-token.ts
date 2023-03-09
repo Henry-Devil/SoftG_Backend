e6bfb6dda125241c9eb5366a54023ba97fef7631
Henry Viloria <henrimavi123@gmail.com>
2023-03-08 19:01:10 -0500

    Tiene token

--- src/routes/validate-token.ts
+++ src/routes/validate-token.ts
@@ -1,9 +1,23 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
 const validateToken = (req: Request, res: Response, next: NextFunction) => {
     const headerToken = req.headers['authorization']
     console.log(headerToken);
-    next()
+
+    if (headerToken != undefined && headerToken.startsWith('Bearer ')){
+        //Tiene token
+        const bearerToken = headerToken.slice(7);
+        console.log(bearerToken);
+        
+        // jwt.verify()
+
+        next()
+    } else {
+        res.status(401).json({
+            msg: "Acceso denegado"
+        })
+    }
 }
 
 export default validateToken;
